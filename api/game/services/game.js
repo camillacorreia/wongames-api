41b6927e358859949217819494929acf266add94
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-20 12:52:45 -0300

    feat: add create games

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -39,6 +39,40 @@ async function create(name, entityName) {
   }
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 async function createManyToManyData(products) {
   const developers = {};
   const publishers = {};
@@ -77,5 +111,6 @@ module.exports = {
     } = await axios.get(gogApiUrl);
 
     await createManyToManyData([products[3], products[4]]);
+    await createGames([products[3], products[4]]);
   }
 };
